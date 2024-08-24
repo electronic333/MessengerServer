25da8755df9fef264944c4833113bfc3c349e9c4
Alexunder <alex.skaut2@yandex.ru>
2024-08-24 19:55:56 +0700

    Now json ignored technical properties

--- MessengerServer/Models/Database/Chat.cs
+++ MessengerServer/Models/Database/Chat.cs
@@ -1,7 +1,13 @@
-﻿namespace MessengerServer.Models.Database;
+﻿using System.Text.Json.Serialization;
+
+namespace MessengerServer.Models.Database;
 
 public class Chat {
   public int Id { get; set; }
+
+  [JsonIgnore]
   public IEnumerable<User> Users { get; set; } = default!;
+
+  [JsonIgnore]
   public IEnumerable<Message> Messages { get; set; } = default!;
 }
--- MessengerServer/Models/Database/User.cs
+++ MessengerServer/Models/Database/User.cs
@@ -1,9 +1,15 @@
-﻿namespace MessengerServer.Models.Database;
+﻿using System.Text.Json.Serialization;
+
+namespace MessengerServer.Models.Database;
 
 public class User {
   public int Id { get; set; }
   public string Name { get; set; } = "";
   public string Email { get; set; } = "";
+
+  [JsonIgnore]
   public IEnumerable<Chat> Chats { get; set; } = default!;
+
+  [JsonIgnore]
   public IEnumerable<Message> Messages { get; set; } = default!;
 }
