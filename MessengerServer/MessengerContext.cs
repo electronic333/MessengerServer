e396ca343412f5f7d611aeb36d6de0524759fe90
Alexunder <alex.skaut2@yandex.ru>
2024-08-26 12:14:55 +0700

    Rename context

---
+++ MessengerServer/MessengerContext.cs
@@ -0,0 +1,37 @@
+﻿using MessengerServer.Models.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace MessengerServer;
+
+public class MessengerContext : DbContext {
+  private readonly ILogger _logger;
+
+  public DbSet<User> Users { get; set; } = default!;
+  public DbSet<Chat> Chats { get; set; } = default!;
+  public DbSet<Message> Messages { get; set; } = default!;
+  public DbSet<Invite> Invites { get; set; } = default!;
+
+  public MessengerContext (ILogger<MessengerContext> logger, DbContextOptions<MessengerContext> options) 
+    : base(options)  
+  {
+    _logger = logger;
+
+    bool created = Database.EnsureCreated();
+
+    if (created) {
+      _logger.LogInformation("Database created.");
+    }
+    else {
+      _logger.LogInformation("Connected to existed database.");
+    }
+  }
+
+  protected override void OnModelCreating (ModelBuilder modelBuilder) {
+    modelBuilder.Entity<User>(user => {
+      user.HasIndex(u => u.Name).IsUnique();
+      user.HasIndex(u => u.Email).IsUnique();
+    });
+
+    base.OnModelCreating(modelBuilder);
+  }
+}
