185a8c944ad3b59657dd937273d2eb5dd8037573
Alexunder <alex.skaut2@yandex.ru>
2024-08-28 02:08:10 +0700

    Add database migrations

---
+++ MessengerServer/Extensions/MigrationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MessengerServer.Extensions;
+
+public static class MigrationExtensions {
+
+  public static void ApplyMigrations (this IApplicationBuilder app) {
+    using var scope = app.ApplicationServices.CreateScope();
+    using var context = scope.ServiceProvider.GetRequiredService<MessengerContext>();
+
+    context.Database.Migrate();
+  }
+
+}
