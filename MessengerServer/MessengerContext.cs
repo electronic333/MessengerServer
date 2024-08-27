e06dd96bd9c0e1631c5366b5d9b44e978bc331f6
Alexunder <alex.skaut2@yandex.ru>
2024-08-28 00:06:47 +0700

    Rename database context

--- MessengerServer/MessengerContext.cs
+++ MessengerServer/MessengerContext.cs
@@ -15,7 +15,7 @@ public class MessengerContext : DbContext {
     : base(options)  
   {
     _logger = logger;
-
+    
     bool created = Database.EnsureCreated();
 
     if (created) {
@@ -31,6 +31,13 @@ public class MessengerContext : DbContext {
       user.HasIndex(u => u.Name).IsUnique();
       user.HasIndex(u => u.Email).IsUnique();
     });
+
+    modelBuilder.Entity<User>().HasMany(u => u.ChatsMember).WithMany(c => c.Users);
+
+    modelBuilder.Entity<Chat>().HasOne(c => c.Creator).WithMany(u => u.ChatsOwn);
+
+    modelBuilder.Entity<Invite>().HasOne(i => i.Sender).WithMany(u => u.OutgoingInvites);
+    modelBuilder.Entity<Invite>().HasOne(i => i.Receiver).WithMany(u => u.IncomingInvites);
 
     base.OnModelCreating(modelBuilder);
   }
